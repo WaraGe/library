39eb91b3d1f88a8d5b9934045d073fb965da1200
zpfhfh774 <dltjddnr774@gmail.com>
2023-01-26 21:51:52 +0900

    ﻿도서정보 조회 페이지번호 정리

--- library/src/main/java/com/korit/library/repository/BookRepository.java
+++ library/src/main/java/com/korit/library/repository/BookRepository.java
@@ -26,6 +26,7 @@ public interface BookRepository {
     U 도서수정
     D 도서삭제
      */
+    public int getBookTotalCount(SearchNumberListReqDto searchNumberListReqDto);
     public List<BookMst> searchBook(SearchReqDto searchReqDto);
     public BookMst findBookByBookCode(String bookCode);
 
--- library/src/main/resources/static/js/admin/book_search.js
+++ library/src/main/resources/static/js/admin/book_search.js
@@ -1,11 +1,12 @@
 window.onload = () => {
   BookService.getInstance().loadBookList();
   BookService.getInstance().loadCategories();
+  BookService.getInstance().loadSearchNumberList();
   ComponentEvent.getInstance().addClickEventSearchButton();
 }
 
 let searchObj = {
-  page : 1,
+  page : 5,
   category : "",
   searchValue : "",
   order : "bookId",
@@ -62,7 +63,32 @@ class BookSearchApi {
     return returnData;
   }
 
+  getBookTotalCount(searchObj) {
+    let returnData = null;
+
+    $.ajax({
+        async: false,
+        type: "get",
+        url: "http://127.0.0.1:8000/api/admin/books/totalcount",
+        data: {
+          "category" : searchObj.category,
+          "searchValue": searchObj.searchValue
+        },
+        dataType: "json",
+        success: response => {
+            console.log(response);
+            returnData = response.data;
+        },
+        error: error => {
+            console.log(error);
+        }
+    });
+
+    return returnData;
 }
+
+}
+
 
 class BookService {
   static #instance = null;
@@ -92,11 +118,78 @@ class BookService {
           <td>${data.category}</td>
           <td>${data.rentalStatus == "Y" ? "대여중" : "대여가능"}</td>
           <td><i class="fa-solid fa-square-pen"></i></td>
-          <td><i class="fa-solid fa-square-minus"></i></td>
         </tr>
         `;
       });
+      this.loadSearchNumberList();
     }
+
+    loadSearchNumberList() {
+      const pageController = document.querySelector(".page-controller");
+      pageController.innerHTML = "";
+
+      const totalCount = BookSearchApi.getInstance().getBookTotalCount(searchObj);                      // Math.floor (값) 소수점단위를 버림
+      const maxPageNumber = totalCount % searchObj.count == 0 
+                        ? Math.floor(totalCount / searchObj.count) 
+                        : Math.floor(totalCount / searchObj.count) + 1;
+
+// javascript:void(0) => 클릭이벤트는 살리면서 href기능을 상실하게 하는방법!
+      pageController.innerHTML = `
+          <a href="javascript:void(0)" class="pre-button disabled">이전</a>
+          <ul class="page-numbers">
+            
+          </ul>
+          <a href="javascript:void(0)" class="next-button disabled">다음</a>
+      `;
+
+      if(searchObj.page != 1) { //감소
+        const preButton = pageController.querySelector(".pre-button")
+        preButton.classList.remove("disabled");
+        
+        preButton.onclick = () => {
+          searchObj.page--;
+
+          this.loadBookList();
+        }
+      }
+      if(searchObj.page != maxPageNumber) { //증가
+        const nextButton = pageController.querySelector(".next-button")
+        nextButton.classList.remove("disabled");
+        
+        nextButton.onclick = () => {
+          searchObj.page++;
+
+          this.loadBookList();
+        }
+      }
+
+      const startIndex = searchObj.page % 5 == 0 
+                        ? searchObj.page - 4 
+                        : searchObj.page - (searchObj.page % 5) + 1;
+      const endIndex = startIndex + 4 <= maxPageNumber 
+                        ? startIndex + 4
+                        : maxPageNumber;
+      const pageNumbers = document.querySelector('.page-numbers');
+      
+
+      for(let i = startIndex; i <= endIndex; i++) {
+        pageNumbers.innerHTML += `
+        <a href="javascript:void(0)"class="page-button ${i == searchObj.page ? "disabled" : ""}"><li>${i}</li></a>`
+      }
+
+      const pageButtons = document.querySelectorAll('.page-button');
+      pageButtons.forEach((button) => {
+        const pageNumber = button.textContent;
+        if(pageNumber != searchObj.page) {
+          button.onclick = () => {
+            searchObj.page = pageNumber;
+            this.loadBookList(); // page값을 덮어씨우고 새로 불러옴
+          }
+        } 
+      })
+    }
+    
+
     loadCategories() {
       const responseData = BookSearchApi.getInstance().getCategories();
 
@@ -127,7 +220,7 @@ class ComponentEvent {
       searchButton.onclick = () => {
         searchObj.category = categorySelect.value;
         searchObj.searchValue = searchInput.value;
-
+        searchObj.page = 1; //  검색을 한다면 1번페이지로 이동
         BookService.getInstance().loadBookList();
         console.log("검색실행");
       }
