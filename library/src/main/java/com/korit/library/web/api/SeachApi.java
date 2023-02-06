6d1ef9f4d689a171a5043e76656e6390e95e7f26
zpfhfh774 <dltjddnr774@gmail.com>
2023-02-06 21:53:02 +0900

    검색사이트 구현#2

--- library/src/main/java/com/korit/library/web/api/SeachApi.java
+++ library/src/main/java/com/korit/library/web/api/SeachApi.java
@@ -1,8 +1,10 @@
 package com.korit.library.web.api;
 
+import com.korit.library.service.SearchService;
 import com.korit.library.web.dto.CMRespDto;
 import com.korit.library.web.dto.SearchBookReqDto;
 import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "도서 검색 API")
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class SeachApi {
+
+
+    private final SearchService searchService;
 
     @GetMapping("/search")
     public ResponseEntity<CMRespDto<?>> search(SearchBookReqDto searchBookReqDto) {
 
         return ResponseEntity
                 .ok()
-                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", null));
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully"
+                        , searchService.getSearchBooks(searchBookReqDto)));
+    }
+
+    @GetMapping("/search/totalcount")
+    public ResponseEntity<CMRespDto<Integer>> getSearchBookTotalCount(SearchBookReqDto searchBookReqDto) {
+        return ResponseEntity.ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(),
+                        "Successfully", searchService.getSearchTotalCount(searchBookReqDto)));
     }
 }
--- library/src/main/resources/static/js/search/search.js
+++ library/src/main/resources/static/js/search/search.js
@@ -1,4 +1,6 @@
 window.onload = () => {
+  SearchService.getInstance().clearBookList();
+  SearchService.getInstance().loadSearchBooks();
   SearchService.getInstance().loadCategories();
   
   ComponentEvent.getInstance().addClickEventCategoryCheckboxs(); 
@@ -38,6 +40,44 @@ class SearchApi {
 
     return returnData;
   }
+  getTotalCount() {
+    let responseData = null;
+
+    $.ajax({
+      async: false,
+      type: "get",
+      url: "http://127.0.0.1:8000/api/search/totalcount",
+      data: searchObj,
+      dataType: "json",
+      success: (response) => {
+        responseData = response.data;
+        console.log(`검색한 갯수: ${responseData}`);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+    return responseData;
+  }
+
+  searchBook() {
+    let responseData = null;
+
+    $.ajax({
+      async: false,
+      type: "get",
+      url: "http://127.0.0.1:8000/api/search",
+      data: searchObj,
+      dataType: "json",
+      success: (response) => {
+        responseData = response.data;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+    return responseData;
+  }
 }
 
 class SearchService {
@@ -61,6 +101,45 @@ class SearchService {
           <label for="${categoryObj.category}">${categoryObj.category}</label>
         </div>
       `;
+    })
+  }
+
+// 검색이 된다던지, 페이지 새로고침시 실행
+  clearBookList() {
+    const contentFlex = document.querySelector(".content-flex");
+    contentFlex.innerHTML = "";
+  }
+  
+  loadSearchBooks() {
+    const responseData = SearchApi.getInstance().searchBook();
+    const contentFlex = document.querySelector(".content-flex");
+
+    responseData.forEach(data => {
+      contentFlex.innerHTML += `
+        <div class="info-container">
+              <div class="book-desc">
+                <div class="img-container">
+                  <img src="http://127.0.0.1:8000/image/book/${data.saveName != null ? data.saveName : "noimg.gif"}" class="book-img">
+                </div>
+                <div class="like-info"><i class="fa-regular fa-thumbs-up"></i><span class="like-count">${data.likeCount != null ? data.likeCount : 0}</span></div>
+              </div>
+              
+              <div class="book-info">
+                <div class="book-code">${data.bookCode}</div>
+                <h3 class="book-name">${data.bookName}</h3>
+                <div class="info-text book-author"><b>저자: </b>${data.author}</div>
+                <div class="info-text book-publisher"><b>출판사: </b>${data.publisher}</div>
+                <div class="info-text book-publicationdate"><b>출판일: </b>${data.publicationDate}</div>
+                <div class="info-text book-category"><b>카테고리: </b>${data.category}</div>
+                <div class="book-buttons">
+                  <button type="button" class="rental-button">대여하기</button>
+                  <button type="button" class="like-button">쪼아요하기</button>
+                </div>
+              </div>
+            </div>
+        </div>
+      `;
+      
     })
   }
 }
